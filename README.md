d6e9f00b082935b429ca9744b2a37c612572e2a3
bbbbbbai <1159287254@qq.com>
2020-12-12 21:52:10 +0800

    webpack处理图片

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
+# 学习webpack
+
 [参考](https://juejin.cn/post/6904538360249843719#heading-7)
+
+
 
 ### 自动生成html插件：html-webpack-plugin
 html-webpack-plugin 插件简化了HTML文件的创建实现，将为你生成一个 HTML5 文件， 其中包括使用 script 标签的 body 中的所有 webpack 包，实现 public 目录下 index.html 不需要手动引入打包后的文件，会被该插件自动生成 HTML 文件引入
@@ -28,3 +32,26 @@ npm install style-loader css-loader -D
 ```
 
 
+### 分离css文件插件
+mini-css-extract-plugin 插件可以将 CSS 提取为独立的文件，对每个包含 css 的 js 文件都会创建一个CSS文件，支持按需加载 css 和 sourceMap，该插件只能用于 webpack4 中
+安装 mini-css-extract-plugin：
+
+```
+npm install mini-css-extract-plugin -D
+```
+
+### webpack处理less文件
+
+less-loader：加载和转译 LESS 文件 入口文件 main.js 中使用 require() 引入less文件 安装 less 和 less-loader：
+
+```
+npm install less less-loader -D
+```
+
+### webpack处理图片
+
+file-loader： 将文件发送到输出文件夹，并返回（相对）URL url-loader： 像 file loader 一样工作，但如果文件小于限制，可以返回 data URL 安装 url-loader 和 file-loader：
+
+```
+npm install url-loader file-loader -D
+```
--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,27 +1,33 @@
-const path = require("path");
+const path = require('path')
 
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 //引入clean-webpack-plugin
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+//引入mini-css-extract-plugin
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
 //导出配置对象
 module.exports = {
-  entry: "./src/main.js",
+  entry: './src/main.js',
   output: {
     //__dirname是nodejs的一个全局变量，指向被执行js文件的绝对路径
-    path: path.resolve(__dirname, "../dist"),
-    filename: "js/bundle.js",
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/bundle.js',
   },
   //mode：打包模式：production（压缩），development（不压缩）
-  mode: "development",
+  mode: 'development',
   //plugins 为插件配置
   plugins: [
     new HtmlWebpackPlugin({
       //html模板文件路径
-      templte: "./public/index.html",
+      templte: './public/index.html',
     }),
     new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      //打包好文件的存放路径和文件名(/dist/css/index.css)
+      filename: 'css/index.css',
+    }),
   ],
   //module：模块加载规则
   module: {
@@ -36,22 +42,33 @@ module.exports = {
         //use：配置转换时使用的loader
         //处理顺序：从右向左执行
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ["@babel/preset-env"],
+            presets: ['@babel/preset-env'],
           },
         },
       },
       {
         //test：标识出被loader进行转换的文件
-        //正则表达式，匹配所有css文件
-        test: /\.css$/,
+        //正则表达式，匹配所有css和less文件
+        test: /\.(css|less)$/,
         //use：配置转换时使用的loader
         //处理顺序：从右向左执行
         //css-loader：让webpack能够识别解析css文件
+        //less-loader：让webpack能够识别解析css文件
         //style-loader：通过js动态创建style标签，让解析后的css作用到页面中
-        use: ["style-loader", "css-loader"],
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              //公共资源加载路径
+              publicPath: '../',
+            },
+          },
+          'css-loader',
+          'less-loader',
+        ],
       },
     ],
   },
-};
+}
